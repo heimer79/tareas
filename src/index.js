a707a13b133a6f3b1de68bbf12a0ed2017035240
Heimer Martinez <hmartinez@cafetosoftware.com>
2022-10-21 10:45:47 -0500

    init el repo

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {saludar} from './js/componentes';
+import './styles.css';
+
+const nombre = 'jue jue';
+saludar(nombre);
---
+++ src/js/componentes.js
@@ -0,0 +1,16 @@
+import '../css/componentes.css';
+import webpacklogo from '../assets/img/webpack-logo.png';
+
+export const saludar = (nombre) => {
+	/* console.log(`creado etiqueta h1 para ${nombre}`);
+
+	const h1 = document.createElement('h1');
+
+	h1.innerText = `Hola, heimer ${nombre}`;
+
+	document.body.append(h1);
+
+	const img = document.createElement('img');
+	img.src = webpacklogo;
+	document.body.append(img); */
+};
