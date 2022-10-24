d35188a2505748321dc7b62778d28a206c2e7da3
Heimer Martinez <hmartinez@cafetosoftware.com>
2022-10-24 07:43:07 -0500

    creacion del todo list class

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import './styles.css';
-import {Todo} from './classes/todo.class';
+
+import {Todo, TodoList} from './classes';
+
+const todoList = new TodoList();
 
 const tarea = new Todo('aprender javascript');
+const tarea2 = new Todo('comprar un unicornio');
 
-console.log(tarea);
+/* console.log(tarea); */
+
+todoList.nuevoTodo(tarea);
+todoList.nuevoTodo(tarea2);
+
+console.log(todoList);
 
 /* import {saludar} from './js/componentes'; */
 
